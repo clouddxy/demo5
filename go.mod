2d634bb3b329c31685b77dda12c2551888f975c6
daixiongyi <daixiongyi@bytedance.com>
2023-10-08 14:24:45 +0800

    feat: add demo5
    Change-Id: I9cedf825dd830e0f751989e8e1bfd755651ddfe9

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/clouddxy/demo5
+
+go 1.18
---
+++ helper/helper.go
@@ -0,0 +1,9 @@
+package helper
+
+func AddDemo5(a, b int) int {
+	return a + b
+}
+
+func AddInt64Demo5(a, b int64) int64 {
+	return a + b
+}
